00f57e2aa20a65e331d20deec747b680be59075f
Antonio Dias Filho <165062482+antoniodiasfilho@users.noreply.github.com>
2024-04-26 14:00:04 -0300

    criado mais alguns exercicios

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60
+
+console.log(`Sua  velocidade é de ${vel}KM/H`)
+
+if (vel > 60) {
+    console.log('Voce foi multado')
+}
+
+console.log('Use cinto de segurança')
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Voce é Brasleiro')
+} else {
+    console.log('Voce é estrangeiro')
+}
