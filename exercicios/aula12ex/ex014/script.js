1bd2e0f342c1f6a7fe6c81ff36bf350c0feabc59
Antonio Dias Filho <165062482+antoniodiasfilho@users.noreply.github.com>
2024-04-30 11:38:59 -0300

    ex014 finalizado

---
+++ exercicios/aula12ex/ex014/script.js
@@ -0,0 +1,26 @@
+function carregar() {
+    
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} Horas.`
+
+    if (hora >= 0 && hora < 12 ) {
+
+        img.src = 'manhã1.png'
+        document.body.style.background = '#e2cd9f'
+
+    }else if (hora >= 12 && hora < 18 ) {
+
+        img.src = 'tarde2.png'
+        document.body.style.background = '#b9846f'
+
+    }else {
+
+        img.src = 'noite2.png'
+        document.body.style.background = '#515154'
+
+    }
+}
